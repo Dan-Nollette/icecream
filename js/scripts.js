8530eff92463baf65253573c95bca9d4ccd472ad
Dan Nollette and safiy cham <safiy@>
2017-08-14 11:10:15 -0700

    practice adding jquery loop as html elements

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  var flavors = ["chocolate", "vanilla", "pistachio", "mint"];
+  flavors.forEach(function(flavor){
+    $("ul").append("<li>" + flavor + "</li>");
+  });
+
+
+
+
+
+
+
+});
